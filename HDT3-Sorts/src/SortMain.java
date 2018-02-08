ed3e8a641b21439716ff6d448e3a79bb8a045cf1
ClaudeCalewaert <claudecalewaert86@hotmail.com>
2018-02-08 13:38:52 -0600

    Codigo para Generar numeros aleatorios

---
+++ HDT3-Sorts/src/SortMain.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Antonio
+ */
+public class SortMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Sort main = new Sort();
+        main.generarNumeros("texto.txt");
+        //System.out.println(main.numeros[2999]);
+    }
+    
+}
